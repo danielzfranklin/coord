bce4212bcbcb7ff4e65939f4b03ba4057677a335
Daniel Franklin <daniel@danielzfranklin.org>
2020-08-22 00:34:31 +0100

    Add round trip proptests for points
    Currently UTM -> LatLng -> UTM is commented out b/c of a bug.

--- test/point/point_prop_test.exs
+++ test/point/point_prop_test.exs
@@ -4,6 +4,36 @@ defmodule CoordProp.Point do
   import PointHelpers
   use Coord
 
+  describe "conversion between points should round trip when going" do
+    # TODO: get this check to work.
+    # The issue is with the utm generator, it generates invalid eastings which
+    # reach outside of the zone. These get converted into valid LatLng, which
+    # are then converted into a valid UTM which is the correct way of
+    # representing the original utm.
+    #
+    # To fix this we need to better validate eastings in the UTM generator.
+    # See the bug report I filed with Geodesy
+    # <https://github.com/chrisveness/geodesy/issues/86>
+
+    # property "UTM -> LatLng -> UTM" do
+    #   forall start <- utm() do
+    #     start
+    #     |> LatLng.from()
+    #     |> UTM.from()
+    #     |> assert_points_approx_eq(start)
+    #   end
+    # end
+
+    property "LatLng -> UTM -> LatLng" do
+      forall start <- latlng_that_can_be_converted_to_utm() do
+        start
+        |> UTM.from()
+        |> LatLng.from()
+        |> assert_points_approx_eq(start)
+      end
+    end
+  end
+
   describe "conversion should give same result as reference for" do
     property "LatLng -> UTM" do
       forall latlng_point <- latlng_that_can_be_converted_to_utm() do
